b519f74cf832ee2167f78e597c84eeb15306cf02
Mason Gallo <mason@Masons-MacBook-Pro.local>
2015-04-16 23:40:19 -0400

    Starting initial skeleton with comments

---
+++ linearRegression.R
@@ -0,0 +1,9 @@
+# include date and author name
+# include name and location of all files (.csv etc)
+# use shasum on the files to ensure working off same file
+
+# initial exploration of data with summary and str
+
+# check linearity assumption
+
+# transform variables where needed
